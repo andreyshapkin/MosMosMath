fc45ef61d615092bfd387090670ee826c7b2fb7b
agshapki <andrey.shapkin@gmail.com>
2018-08-05 00:29:13 -0700

    First revision of the MosMos

---
+++ app/src/main/java/MainMathClass.java
@@ -0,0 +1,11 @@
+public class MainMathClass {
+    enum OperationType {
+        ADD_10,
+        SUB_10
+    }
+
+    OperationType operationType;
+    Integer op1;
+    Integer op2;
+    Integer result;
+}
---
+++ app/src/main/java/com/example/agshapki/mosmos/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.agshapki.mosmos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(),"main activity has started", Toast.LENGTH_LONG).show();
+
+    }
+}
