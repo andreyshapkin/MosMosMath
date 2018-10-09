56626af1742a52cfe210740327ef9240f6a4accf
agshapki <andrey.shapkin@gmail.com>
2018-10-08 20:34:56 -0700

    release changes

--- app/src/androidTest/java/com/mosmos/mosmos_math/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mosmos/mosmos_math/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.agshapki.mosmos", appContext.getPackageName());
+        assertEquals("com.mosmos.mosmos_math", appContext.getPackageName());
     }
 }
